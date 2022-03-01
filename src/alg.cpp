60cc1140bb5a145901c79d0f362136f304b6642d
Golikova Dasha <99117131+golikovaDasha@users.noreply.github.com>
2022-03-01 19:41:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,30 +4,24 @@
 
 
 double pown(double value, uint16_t n) {
-if (0 == n) {
+if (n == 0) {
   return 1;
 }
-if (1 == n) {
+if (n == 1) {
   return value;
 } else {
   return value*pown(value, n-1);
 }
 }
 uint64_t fact(uint16_t n) {
-if (n == 0) {
+if (n <= 1) {
   return 1;
-}
-while (n != 0) {
+} else {
   return n*fact(n - 1);
 }
 }
 double calcItem(double x, uint16_t n) {
-if (n == 0 || x == 0) {
-  return 1;
-}
-while ((n != 0) || (x != 0)) {
-  return pown(x, n) / fact(n);
-}
+return pown(x, n) / fact(n);
 }
 double expn(double x, uint16_t count) {
 double s = 1;
@@ -38,13 +32,13 @@ return s;
 }
 double sinn(double x, uint16_t count) {
 double s = 0;
-for (int i = count; i > 0; i -= 2) {
-  return s+=pown(-1, i - 1) * pown(x, 2 * i - 1) / fact(2*i-1);
+for (int i = count; i > 0; i--) {
+  return s+=pown((-1), i - 1) * pown(x, 2 * i - 1) / fact(2*i-1);
 }
 }
 double cosn(double x, uint16_t count) {
 double s = 0;
-for (int i = count; i > 0; i -= 2) {
-  return s += pown(-1, i - 1) * pown(x, 2 * i - 2) / fact(2*i-2);
+for (int i = count; i > 0; i--) {
+  return s += pown((-1), i - 1) * pown(x, 2 * i - 2) / fact(2*i-2);
   }
 }
